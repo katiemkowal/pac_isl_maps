e8177f73f599a6027ab1a906ccbece5d38d1861c
Katie Kowal <169676572+katiemkowal@users.noreply.github.com>
2024-05-30 13:21:35 +0000

    first commit

---
+++ analysis/xcast/plot_cons.py
@@ -0,0 +1,17 @@
+import xcast as xc
+import xarray as xr
+import cartopy.crs as ccrs
+import numpy as np
+import matplotlib.pyplot as plt
+import pacisl_config as cfg
+import os
+
+fmodel1 = xr.open_dataset('./gefs_week34_cons_tercile.nc')
+fmodel = fmodel1.precip
+fmodel = fmodel.sel(lon=slice(0.01,179), lat=slice(-20,-16))#.rename({'time':'T', 'lon': 'LON', 'lat': 'LAT'})
+
+os.path.join(cfg.output_path,'gefs_week_34_cons.png')
+
+print (fmodel.isel(time=0))
+from mprob import mview_probabilistic
+mview_probabilistic(fmodel.isel(time=0), title='GEFS, Week-34, S. Weighted Cons., Valid: 13Jun2024 - 26Jun2024', savefig=os.path.join(cfg.output_path,'gefs_week_34_cons.png'))
---
+++ analysis/xcast/temp_epoelm.ctl
@@ -0,0 +1,16 @@
+dset temp_epoelm.nc
+title model
+undef 9.96921e+36
+dtype netcdf
+xdef 9 linear 175 .5
+ydef 9 linear -20 .5
+zdef 1 linear 0 1
+tdef 1 linear 00Z10Oct2023 1mn
+edef 3
+1 1 00Z10Oct2023
+2 1 00Z10Oct2023
+3 1 00Z10Oct2023
+endedef
+vars 1
+prob=>prob  0  y,x,t,e  probability
+endvars
