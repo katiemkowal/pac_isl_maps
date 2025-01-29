2155756687671d9857af1e0aac1ad170c1ad62a9
Katie Kowal <169676572+katiemkowal@users.noreply.github.com>
2025-01-29 16:28:30 +0000

    automated commit by cron job on Wed Jan 29 16:28:30 UTC 2025

--- stations/station_image_prep.sh
+++ stations/station_image_prep.sh
@@ -34,9 +34,9 @@ main(){
     # gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/cmorph30percent.tif /cpc/int_desk/pac_isl/stations/images/cmorph30percent_tiles
     # gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/cmorph7anom.tif /cpc/int_desk/pac_isl/stations/images/cmorph7anom_tiles
     # gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/cmorph7total.tif /cpc/int_desk/pac_isl/stations/images/cmorph7total_tiles
-    # gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/gefswk1pcons.tif /cpc/int_desk/pac_isl/stations/images/gefswk1pcons_tiles
+    gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/gefswk1pcons.tif /cpc/int_desk/pac_isl/stations/images/gefswk1pcons_tiles
      # gdal2tiles.py -p mercator -z 0-5 /cpc/int_desk/pac_isl/stations/images/gefswk2pcons.tif /cpc/int_desk/pac_isl/stations/images/gefswk2pcons_tiles
-    gdal2tiles.py -p mercator -z 0-5 https://ftp.cpc.ncep.noaa.gov/fews/DroughtMonitor/gis/sri/20230205/sri1_glb.tif /cpc/int_desk/pac_isl/stations/images/gefswk1pcons_tiles
+    # gdal2tiles.py -p mercator -z 0-5 https://ftp.cpc.ncep.noaa.gov/fews/DroughtMonitor/gis/sri/20230205/sri1_glb.tif /cpc/int_desk/pac_isl/stations/images/gefswk1pcons_tiles
 
     current_branch=temp-branch
     log "Current branch: $current_branch"
@@ -55,4 +55,4 @@ main(){
 }
 
 main
-             
+             https://ftp.cpc.ncep.noaa.gov/fews/DroughtMonitor/gis/sri/20230205/sri1_glb.tif
